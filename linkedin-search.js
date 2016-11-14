0bfcdd5e355b207d1a001606c89d152dee3d1d0c
Matias Lespiau <lespiau@gmail.com>
2016-11-14 18:27:26 -0300

    Log more information

--- linkedin-search.js
+++ linkedin-search.js
@@ -1,5 +1,8 @@
 var tableElements = document.querySelectorAll('.sortable tr');
 var columns = tableElements[0];
-for (var i; i < columns.length; ++i) {
+for (var i = 0; i < columns.length; ++i) {
     console.log(columns[i]);
+}
+for (var j = 1; j < tableElements.length; ++j) {
+    console.log(tableElements[j]);
 }
